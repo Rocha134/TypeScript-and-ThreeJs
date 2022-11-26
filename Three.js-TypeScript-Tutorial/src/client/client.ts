b1d9ebda3b889c6f89d179fcbb7183023567d38b
Rocha134 <rochapakito@gmail.com>
2022-11-25 18:38:58 -0600

    Tutoring

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -7,18 +7,29 @@ import Stats from 'three/examples/jsm/libs/stats.module.js'
 //We import the dat.gui library
 import { GUI } from 'dat.gui'
 
+//We do the fetch to get the data from the server
+var baseURL = "http://localhost:5000"
+
+fetch(baseURL + "/games", {
+  method: "POST"
+}).then(response => {
+  var location2 = response.headers.get('Location');
+  console.log(location2);
+});
+
 const scene = new THREE.Scene()
 //Add an axes helper to the scene
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
 
 const camera = new THREE.PerspectiveCamera(
-    75,
+    100,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
 )
-camera.position.z = 2
+camera.position.z = 20
+camera.rotation.y = 20
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -27,13 +38,91 @@ document.body.appendChild(renderer.domElement) //Add the renderer to the body of
 const controls = new OrbitControls(camera, renderer.domElement) //OrbitControls
 //controls.addEventListener('change', render) // use if there is no animation loop. The first parameter is the event type, the second is the callback function.
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
+// //We create a grid plane
+// const gridHelper = new THREE.GridHelper(60, 60)
+// scene.add(gridHelper)
+//We create the floor
+const floorGeometry = new THREE.PlaneGeometry(30, 30, 30, 30)
+const floorMaterial = new THREE.MeshBasicMaterial({
+    //We set the color of the floor to white
+    color: 0xffffff,
+    side: THREE.DoubleSide,
 })
+//We add a texture to the floor
+const floorTexture = new THREE.TextureLoader().load(
+    'https://threejsfundamentals.org/threejs/resources/images/checker.png'
+)
+floorTexture.wrapS = THREE.RepeatWrapping
+floorTexture.wrapT = THREE.RepeatWrapping
+floorTexture.magFilter = THREE.NearestFilter
+floorTexture.repeat.set(15, 15)
+floorMaterial.map = floorTexture
+const floor = new THREE.Mesh(floorGeometry, floorMaterial)
+floor.rotation.x = Math.PI / 2
+// //We move the floor 30 units to the right
+floor.position.x = 15
+// //We move the floor 30 units to the front
+floor.position.z = 15
+scene.add(floor)
+//We create a cube to represent the robot
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x3c4d69 })
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+const cube2 = new THREE.Mesh(cubeGeometry, cubeMaterial)
+const cube3 = new THREE.Mesh(cubeGeometry, cubeMaterial)
+const cube4 = new THREE.Mesh(cubeGeometry, cubeMaterial)
+//scene.add(cube)
 
-const cube = new THREE.Mesh(geometry, material)
+//We create a function to add a wall with texture to the scene
+function addWall(x: number, y: number, z: number, width: number, height: number) {
+    const wallGeometry = new THREE.PlaneGeometry(width, height)
+    const loader = new THREE.TextureLoader();
+    const wallMaterial = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        side: THREE.DoubleSide,
+    })
+    //We set the texture of the wall from a file
+    const wallTexture = new THREE.TextureLoader().load(
+        'https://threejsfundamentals.org/threejs/resources/images/checker.png'
+    )
+    wallTexture.wrapS = THREE.RepeatWrapping
+    wallTexture.wrapT = THREE.RepeatWrapping
+    wallTexture.magFilter = THREE.NearestFilter
+    wallTexture.repeat.set(15, 15)
+    wallMaterial.map = wallTexture
+    const wall = new THREE.Mesh(wallGeometry, wallMaterial)
+    wall.position.x = x
+    wall.position.y = y
+    wall.position.z = z
+    //We translate the wall to the right by half its width - 0.5
+    wall.translateX(width / 2 - 0.5)
+    //We translate the wall up by half its height - 0.5
+    wall.translateY(height / 2 - 0.5)
+    scene.add(wall)
+    return wall
+}
+
+// //Lateral walls
+// addWall(0.5, 0.5, 0, 30, 10)
+// addWall(0.5, 0.5, 30, 30, 10)
+// //Frontal walls
+// const wall1 = addWall(15.5, 0.5, 15, 30, 10)
+// wall1.rotation.y = Math.PI / 2
+// const wall2 = addWall(-14.5, 0.5, 15, 30, 10)
+// wall2.rotation.y = Math.PI / 2
+
+
+cube.position.x = 0+0.5;
+cube.position.y = 0.5;
+cube.position.z = 0+0.5;
+
+// cube2.position.x = 0+0.5;
+// cube2.position.y = 0.5;
+// cube2.position.z = 0+0.5;
+
+//scene.add(cube2)
+
+// const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
 console.dir(scene)
@@ -52,22 +141,22 @@ document.body.appendChild(stats.dom) //Add the stats to the body of the html
 const gui = new GUI() //Dat.gui
 const cubeFolder = gui.addFolder('Cube') //Add a cubeFolder to the dat.gui
 cubeFolder.add(cube.position, 'x', -5, 5, 0.01) //Add the x position to the dat.gui
-cubeFolder.add(cube.position, 'y', -5, 5, 0.01) //Add the y position to the dat.gui
+// cubeFolder.add(cube.position, 'y', -5, 5, 0.01) //Add the y position to the dat.gui
 cubeFolder.add(cube.position, 'z', -5, 5, 0.01) //Add the z position to the dat.gui
-cubeFolder.add(cube.rotation, 'x', 0, 2 * Math.PI, 0.01) //Add the x rotation to the dat.gui
-cubeFolder.add(cube.rotation, 'y', 0, 2 * Math.PI, 0.01) //Add the y rotation to the dat.gui
-cubeFolder.add(cube.rotation, 'z', 0, 2 * Math.PI, 0.01) //Add the z rotation to the dat.gui
-cubeFolder.add(cube.scale, 'x', 0, 2, 0.01) //Add the x scale to the dat.gui
-cubeFolder.add(cube.scale, 'y', 0, 2, 0.01) //Add the y scale to the dat.gui
-cubeFolder.add(cube.scale, 'z', 0, 2, 0.01) //Add the z scale to the dat.gui
-cubeFolder.add(cube, 'visible') //Add the visible property to the dat.gui
-cubeFolder.add(cube, 'castShadow') //Add the castShadow property to the dat.gui
-cubeFolder.add(cube, 'receiveShadow') //Add the receiveShadow property to the dat.gui
+// cubeFolder.add(cube.rotation, 'x', 0, 2 * Math.PI, 0.01) //Add the x rotation to the dat.gui
+// cubeFolder.add(cube.rotation, 'y', 0, 2 * Math.PI, 0.01) //Add the y rotation to the dat.gui
+// cubeFolder.add(cube.rotation, 'z', 0, 2 * Math.PI, 0.01) //Add the z rotation to the dat.gui
+// cubeFolder.add(cube.scale, 'x', 0, 2, 0.01) //Add the x scale to the dat.gui
+// cubeFolder.add(cube.scale, 'y', 0, 2, 0.01) //Add the y scale to the dat.gui
+// cubeFolder.add(cube.scale, 'z', 0, 2, 0.01) //Add the z scale to the dat.gui
+// cubeFolder.add(cube, 'visible') //Add the visible property to the dat.gui
+// cubeFolder.add(cube, 'castShadow') //Add the castShadow property to the dat.gui
+// cubeFolder.add(cube, 'receiveShadow') //Add the receiveShadow property to the dat.gui
 
 const cameraFolder = gui.addFolder('Camera') //Add a cameraFolder to the dat.gui
 cameraFolder.add(camera.position, 'x', -5, 5, 0.01) //Add the x position to the dat.gui
 cameraFolder.add(camera.position, 'y', -5, 5, 0.01) //Add the y position to the dat.gui
-cameraFolder.add(camera.position, 'z', -5, 5, 0.01) //Add the z position to the dat.gui
+cameraFolder.add(camera.position, 'z', -5, 100, 0.01) //Add the z position to the dat.gui
 cameraFolder.add(camera.rotation, 'x', 0, 2 * Math.PI, 0.01) //Add the x rotation to the dat.gui
 cameraFolder.add(camera.rotation, 'y', 0, 2 * Math.PI, 0.01) //Add the y rotation to the dat.gui
 cameraFolder.add(camera.rotation, 'z', 0, 2 * Math.PI, 0.01) //Add the z rotation to the dat.gui
@@ -77,38 +166,38 @@ cameraFolder.add(camera, 'far', 0, 5, 0.01) //Add the far to the dat.gui
 cameraFolder.add(camera, 'zoom', 0, 5, 0.01) //Add the zoom to the dat.gui
 cameraFolder.add(camera, 'focus', 0, 5, 0.01) //Add the focus to the dat.gui
 cameraFolder.add(camera, 'aspect', 0, 5, 0.01) //Add the aspect to the dat.gui
-cameraFolder.add(camera, 'clearViewOffset') //Add the clearViewOffset to the dat.gui
-cameraFolder.add(camera, 'updateProjectionMatrix') //Add the updateProjectionMatrix to the dat.gui
-cameraFolder.add(camera, 'clearViewOffset') //Add the clearViewOffset to the dat.gui
 
 function animate() {
     requestAnimationFrame(animate)
-
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-    // //Every second change the color of the cube
-    // setTimeout(() => {
-    //     material.color.setHex(Math.random() * 0xffffff)
-    // }, 1000)
-    
     render()
     stats.update()
 }
 
 function render() {
+    // if (location2 != ""){ // if the game has been created
+    //     var res = await fetch(baseURL + location2); // get the game state
+    //     var data = await res.json(); // parse JSON to JS object that contains the positions of the 10 robots in every step
+    //     //console.log(data);
+    //     //We print the positions for the first robot
+    //     console.log(data[0].x);
+    //     console.log(data[0].y);
+    
+    //     //We assign the positions to the robots
+    //     group.position.x = data[0].x;
+    //     group.position.y = data[0].y;
+        
+    //     //console.log(groups);
+    //     //data.map((d) => { // for each robot
+    //     //  var g = groups[d.id]; 
+    //     //  g.position.x = d.x * 3;
+    //     //  g.position.y = d.y * 3;
+    //     //  return;
+    //     //});
+    
+    //   }
     renderer.render(scene, camera)
 }
 
-//Function to turn the cube into a sphere
-function turnIntoSphere() {
-    const sphereGeometry = new THREE.SphereGeometry(1, 32, 32)
-    const sphereMaterial = new THREE.MeshBasicMaterial({
-        color: 0x00ff00,
-        wireframe: false,
-    })
-    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
-    scene.add(sphere)
-}
 
 
 animate()
